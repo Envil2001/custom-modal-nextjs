7ac7c3390418a8589f353079e1a3b23bc61a8e0e
andrii <sherbakov12342@gmail.com>
2024-04-23 10:00:13 +0200

    added react-syntax-highlighter

--- app/ui/home-page.tsx
+++ app/ui/home-page.tsx
@@ -3,15 +3,17 @@
 import { useContext } from "react";
 import { ModalContext } from "../providers";
 import { AdvertisingModal, DefaultModal } from "../widgets/modals";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import atelierCaveDark from "react-syntax-highlighter/dist/esm/styles/hljs/atelier-cave-dark";
 
 export const HomePage = () => {
     const { handleModal } = useContext(ModalContext);
 
     return (
-        <div className="space-y-4">
+        <div className="grid grid-cols-2 gap-6">
             <div>
                 <h1>Default Modal Window</h1>
-                <button className="px-4 rounded-xl font-normal text-sm h-10 bg-default hover:bg-opacity-90 transition-all" onClick={() =>
+                <button className="mb-4 px-4 rounded-xl font-normal text-sm h-10 bg-default hover:bg-opacity-90 transition-all" onClick={() =>
                     handleModal({
                         children: <DefaultModal />,
                         variant: "default",
@@ -20,10 +22,29 @@ export const HomePage = () => {
                 }>
                     Open Default Modal Window
                 </button>
+
+                <SyntaxHighlighter className="rounded-xl" language="react" style={atelierCaveDark}>
+                    {`
+export const HomePage = () => {
+const { handleModal } = useContext(ModalContext);
+  return (
+    <button onClick={() =>
+        handleModal({
+            children: <DefaultModal />,
+            variant: "default",
+            size: "sm",
+        })
+    }>
+        Open Default Modal Window
+    </button>
+  );
+}
+        `}
+                </SyntaxHighlighter>
             </div>
             <div>
                 <h1>Advertising Modal Window</h1>
-                <button className="px-4 rounded-xl font-normal text-sm h-10 bg-default hover:bg-opacity-90 transition-all" onClick={() =>
+                <button className="mb-4 px-4 rounded-xl font-normal text-sm h-10 bg-default hover:bg-opacity-90 transition-all" onClick={() =>
                     handleModal({
                         children: <AdvertisingModal />,
                         variant: "default",
@@ -33,6 +54,26 @@ export const HomePage = () => {
                 }>
                     Open Advertising Modal Window
                 </button>
+
+                <SyntaxHighlighter className="rounded-xl" language="react" style={atelierCaveDark}>
+                    {`
+export const HomePage = () => {
+const { handleModal } = useContext(ModalContext);
+  return (
+    <button onClick={() =>
+        handleModal({
+            children: <AdvertisingModal />,
+            variant: "default",
+            size: "md",
+            closeTimeOut: 5,
+        })
+    }>
+    Open Advertising Modal Window
+    </button>
+  );
+}
+        `}
+                </SyntaxHighlighter>
             </div>
         </div>
     )
